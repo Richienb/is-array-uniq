597ae1bb6358f0f9c152e9565c82253c318a7c6e
Richie Bendall <richiebendall@gmail.com>
2020-11-19 16:26:14 +1300

    Faster similarity test
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 "use strict"
 
 const arrayUniq = require("array-uniq")
-const isObjectEqual = require("fast-deep-equal/es6")
 
 module.exports = array => {
 	if (!Array.isArray(array)) {
 		throw new TypeError(`Expected an array, got ${typeof array}`)
 	}
 
-	return isObjectEqual(array, arrayUniq(array))
+	return array.length === arrayUniq(array).length
 }
